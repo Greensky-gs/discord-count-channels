64a70671c2f28308706068fb7b07c4cc6f1327ce
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-12-05 16:41:21 +0100

    docs: :memo: Update functions doc

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,19 @@
 import { channelCounterTypes } from '../typings/typings';
 
+/**
+ * Count how many times a value is occured in an array
+ * @param array Array to test the values
+ * @param value Value to test in the array
+ * @returns Number of times the value is occured in the array
+ */
 export const countInArray = (array: any[], value: any): number => {
     return array.reduce((n, x) => n + (x === value), 0);
 };
+/**
+ * Get a valid channel order
+ * @param order Order to check
+ * @returns A valid order
+ */
 export const getValidChannelOrder = (order: channelCounterTypes[]): channelCounterTypes[] => {
     if (!order || !(order instanceof Array) || order.length !== 3) return ['all', 'bots', 'humans'];
 
