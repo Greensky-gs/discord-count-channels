6bbe51dee9f98d43f90a5da2f08a151415e245f8
Greensky <david.hesliere@gmail.com>
2023-08-01 13:55:28 +0200

    refactor: :recycle: Rename types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Counter } from './structures/manager';
 export {
     databaseTable as tableType,
-    counterType as channelCounterTypes,
+    counterType,
     configsType,
     countChannelType,
     createCountersType
