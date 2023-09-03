113644c79a612faa065c53f76d60ea2c37f316d2
Greensky <david.hesliere@gmail.com>
2023-09-03 14:19:15 +0200

    chore: :fire: Remove all files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export { Counter } from './structures/manager';
-export {
-    databaseTable as tableType,
-    counterType,
-    configsType,
-    countChannelType,
-    createCountersType
-} from './typings/typings';
-export { countInArray, getValidChannelOrder } from './utils/functions';
